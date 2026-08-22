6b3503b88766efbe0173166002f94812cb7ea0e4
himanshugusn <himanshugusn@gmail.com>
2026-08-22 11:54:12 +0530

    add java-script if-else

---
+++ script.js
@@ -0,0 +1,42 @@
+let num1=5;
+let num2=12;
+let num3=45;
+let num4=34324;
+let num5=3131;
+
+
+if(num1%2==0){
+    console.log(`the number ${num1} is even`)
+}
+else{
+    console.log(`the number ${num1} is odd`)
+
+}
+if(num2%2==0){
+    console.log(`the number ${num2} is even`)
+}
+else{
+    console.log(`the number ${num2} is odd`)
+
+}
+if(num3%2==0){
+    console.log(`the number ${num3} is even`)
+}
+else{
+    console.log(`the number ${num3} is odd`)
+
+}
+if(num4%2==0){
+    console.log(`the number ${num4} is even`)
+}
+else{
+    console.log(`the number ${num4} is odd`)
+
+}
+if(num5%2==0){
+    console.log(`the number ${num5} is even`)
+}
+else{
+    console.log(`the number ${num5} is odd`)
+
+}
