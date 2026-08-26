fcaee131ab9af9d138de122f3ed6f46a54809609
himanshugusn <himanshugusn@gmail.com>
2026-08-26 11:46:36 +0530

    js update

--- script.js
+++ script.js
@@ -1,42 +1,9 @@
-let num1=5;
-let num2=12;
-let num3=45;
-let num4=34324;
-let num5=3131;
+let p = 1000;   // Principal amount
+let r = 0.10;  // Rate of interest (10%)
+let n = 1;     // Compounded once per year
+let t = 5;     // Time in years
 
+let a = p * (1 + (r/n)) ** (n*t);  // Amount formula
+let ci = a - p;                     // Compound Interest = Amount - Principal
 
-if(num1%2==0){
-    console.log(`the number ${num1} is even`)
-}
-else{
-    console.log(`the number ${num1} is odd`)
-
-}
-if(num2%2==0){
-    console.log(`the number ${num2} is even`)
-}
-else{
-    console.log(`the number ${num2} is odd`)
-
-}
-if(num3%2==0){
-    console.log(`the number ${num3} is even`)
-}
-else{
-    console.log(`the number ${num3} is odd`)
-
-}
-if(num4%2==0){
-    console.log(`the number ${num4} is even`)
-}
-else{
-    console.log(`the number ${num4} is odd`)
-
-}
-if(num5%2==0){
-    console.log(`the number ${num5} is even`)
-}
-else{
-    console.log(`the number ${num5} is odd`)
-
-}
+console.log(`The compound interest after ${t} years is: ${ci}`);
